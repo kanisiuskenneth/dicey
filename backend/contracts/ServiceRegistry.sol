2ed36822af6cd0270c7b6b4e6ccea67904ed7848
Kenneth Halim <kanisiuskenneth@gmail.com>
2019-07-11 09:51:35 +0700

    Add advanced network

--- backend/contracts/ServiceRegistry.sol
+++ backend/contracts/ServiceRegistry.sol
@@ -9,6 +9,8 @@ contract ServiceRegistry {
     string category;
     string description;
     uint price;
+    uint ratingTotal;
+    uint ratingCount;
     uint timestamp;
   }
   Service[] services;
@@ -20,14 +22,17 @@ contract ServiceRegistry {
     uint timestamp;
   }
 
+
   uint64 serviceCount;
   uint64 i;
 
   mapping(uint64 => uint64) indexes;
   mapping(uint64 => string) additionalInfos;
   mapping(uint64 => ServiceDescription[]) private descriptions;
+  mapping(uint64 => mapping(address => uint64)) userRate;
   mapping(address => mapping(uint64 => bool)) acl;
   mapping(address => uint[]) public myservices;
+
   address owner = msg.sender;
 
   constructor() public {
@@ -37,7 +42,7 @@ contract ServiceRegistry {
 
 
   function addService(string memory _name, string memory _category, string memory _description, uint _price, string memory additionalInfo, string memory fileName, string memory fileContent)  public returns (Service memory){
-    Service memory created = Service(i, msg.sender, _name, _category, _description, _price, now);
+    Service memory created = Service(i, msg.sender, _name, _category, _description, _price, 0,0,now);
     services.push(created);
     indexes[i] = serviceCount;
     myservices[msg.sender].push(i);
@@ -74,6 +79,7 @@ contract ServiceRegistry {
     return (descriptions[id][version-1]);
 
   }
+
   function deleteService(uint64 id) public {
     uint64 index = indexes[id];
     Service memory deleted = services[index];
@@ -84,6 +90,7 @@ contract ServiceRegistry {
     indexes[latest.id] = index;
     serviceCount--;
   }
+
   function payForService(uint64 id) payable public {
     uint64 index = indexes[id];
     Service memory service = services[index];
@@ -91,10 +98,30 @@ contract ServiceRegistry {
     service.owner.transfer(service.price);
     acl[msg.sender][id] = true;
   }
+
   function updateDescription(uint64 id, string memory fileName, string memory fileDescription) public {
     uint64 index = indexes[id];
     require (msg.sender == services[index].owner, "Not Authorized");
     uint version = descriptions[id].length+1;
     descriptions[id].push(ServiceDescription(version, fileName, fileDescription, now));
+  }
+
+  function rateService(uint64 id, uint64 rate) public {
+    uint64 index = indexes[id];
+    bool allowed = (acl[msg.sender][id] || services[index].price == 0) && services[index].owner != msg.sender;
+    require(allowed, "Access not allowed");
+    uint64 val = rate*100;
+    if(userRate[id][msg.sender] == 0) {
+      services[index].ratingTotal += val;
+      services[index].ratingCount++;
+      userRate[id][msg.sender] = val;
+    } else {
+      services[index].ratingTotal -= userRate[id][msg.sender];
+      services[index].ratingTotal += val;
+      userRate[id][msg.sender] = val;
+    }
+  }
+  function getUserRate(uint64 id) public returns (uint64) {
+    return userRate[id][msg.sender];
   }
 }
